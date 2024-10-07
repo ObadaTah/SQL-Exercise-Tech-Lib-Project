001854579e28dec619f1f4b9e1bdcc699dd31dfa
ObadaTah <obadatahboub20@gmail.com>
2024-10-07 21:18:07 +0300

    added indexes

--- database.sql
+++ database.sql
@@ -35,3 +35,15 @@ GO
 
 ALTER TABLE [loan] ADD FOREIGN KEY ([borrower_id]) REFERENCES [borrower] ([borrower_id])
 GO
+
+CREATE INDEX idx_loan_borrower_id ON [loan](borrower_id);
+CREATE INDEX idx_loan_book_id ON [loan](book_id);
+CREATE INDEX idx_loan_date_borrowed ON [loan](date_borrowed);
+CREATE INDEX idx_loan_due_date_returned ON [loan](due_date, date_returned);
+CREATE INDEX idx_loan_borrower_book ON [loan](borrower_id, book_id);
+
+CREATE INDEX idx_borrower_email ON [borrower](email);
+
+CREATE INDEX idx_book_author ON [book](author);
+CREATE INDEX idx_book_current_status ON [book](current_status);
+
